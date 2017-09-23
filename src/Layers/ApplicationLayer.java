9c9ec9e0f97aa65dfb7913f1397845287c800dd5
madisonkh <31267036+madisonkh@users.noreply.github.com>
2017-09-23 15:02:24 -0400

    Update ApplicationLayer.java
    Added encapsulate method that will be rewritten to override the base layer's implemetation.

--- src/Layers/ApplicationLayer.java
+++ src/Layers/ApplicationLayer.java
@@ -6,4 +6,8 @@ public class ApplicationLayer extends Layer {
 	public ApplicationLayer() {
 		super(LayerType.L5);
 	}
+	
+	public static PacketData encapsulate(PacketData packetData) {
+		return packetData != null ? new PacketData(packetData) : null;
+	}
 }
