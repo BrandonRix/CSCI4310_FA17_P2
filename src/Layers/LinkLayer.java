7e5ce7b19e5411168d9d154ed86a943b071dc659
Tabias Pittman <tabiaspittman94@gmail.com>
2017-09-23 13:31:30 -0400

    added getters and setters of LinkLayer

--- src/Layers/LinkLayer.java
+++ src/Layers/LinkLayer.java
@@ -6,6 +6,15 @@ public class LinkLayer extends Layer {
 	private int address;
 	public LinkLayer(int address) {
 		super(LayerType.L2);
+		this.setAddress(address);
+	}
+	
+	public int getAddress() {
+		return address;
+	}
+	public void setAddress(int address) {
 		this.address = address;
 	}
+	
+	
 }
